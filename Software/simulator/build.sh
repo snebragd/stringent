8e514f44e7e7284ef6632c63bd335ef0ca59e1a5
Fredrik Stridsman <fredrik@Fredriks-MacBook-Pro.local>
2018-06-23 19:58:33 +0200

    Added javac to the build script, made a pointless excuse about the sed-thing
    	modified:   build.sh

--- Software/simulator/build.sh
+++ Software/simulator/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+#Sorry for this. could be made a lot nicer with proper preprocessing, but there was no time/interest when I needed to make this work.
 
 cat ../plotter/data.ino | sed 's/SD\.open/fopen/;s/svgFile.close(/fclose(svgFile/;s/SD\.begin(10)/true/;s/svgFile\.read(/fgetc(svgFile/;s/svgFile\.seek(0)/fseek(svgFile, 0, SEEK_SET)/;s/svgFile\.position()/ftell(svgFile)/;s/svgFile\.seek(/tmp_seek(svgFile, /;s/"1.svg"/strdup("1.svg")/' > /tmp/data.ino
 
 #/gcc -g -o sim sim.c -lm -std=c99
 
 clang -o sim sim.c -lm -std=c99 -I../plotter
-
 chmod +x sim
+
+#build visualizer
+javac *.java
