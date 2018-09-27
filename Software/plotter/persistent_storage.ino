a469a35e11100f75ed5b34cb4cfe458ceb0c18f6
oeloeloel <26174257+oeloeloel@users.noreply.github.com>
2018-09-27 08:31:53 -0400

    Update persistent_storage.ino
    Roughly hacked to work with NodeMCU (not actually sure if any changes here make any difference).

--- Software/plotter/persistent_storage.ino
+++ Software/plotter/persistent_storage.ino
@@ -1,5 +1,25 @@
 #include <EEPROM.h>
 #include "MachineDefs.h"
+
+void eepromSetup(){
+    EEPROM.begin(512);
+    //eepromReset();
+}
+
+void eepromReset(){
+    SER_PRINTLN("EEPROM: Store");
+
+    eepromWriteLong(EEPROM_LEFT_ADDR, 150);
+    eepromWriteLong(EEPROM_RIGHT_ADDR, 150);
+    eepromWriteLong(EEPROM_DISPARITY_ADDR, 300);
+    eepromWriteLong(EEPROM_STOPPED_AT_ADDR, 100);
+    eepromWriteLong(EEPROM_CURRENT_PLOT_ADDR, 100);
+    eepromWriteFloat(EEPROM_PRINT_SIZE_ADDR, 1);
+    eepromWriteFloat(EEPROM_CENTER_X_ADDR, 150);
+    eepromWriteFloat(EEPROM_CENTER_Y_ADDR, 150);
+
+    eepromCommit();
+}
  
 void eepromWriteLong(int addr, long data) {
   for(int b=0 ; b<4 ; b++) {
@@ -24,4 +44,8 @@ long eepromReadLong(int addr) {
 float eepromReadFloat(int addr) {
   long tmp = eepromReadLong(addr);  
   return *(float*)&tmp;
+}
+
+void eepromCommit(){
+  EEPROM.commit();
 }
